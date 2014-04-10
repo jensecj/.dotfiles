88bb2d2fa896f4a9470c0a366f72de196a0a4ece
Jens Jensen <jeggsen@gmail.com>
2014-04-10 09:08:51 +0200

    fixed readme and removed unneeded file

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 * ln -s .dotfiles/gitconfig ~/.gitconfig
 * ln -s .dotfiles/xinitrc ~/.xinitrc
 * ln -s .dotfiles/Xresources ~/.Xresources
-
 * ln -s .dotfiles/awesomewm/zenburn ~/.config/awesome/themes/zenburn
 * ln -s .dotfiles/awesomewm/rc.lua ~/.config/awesome/rc.lua
 * ln -s .dotfiles/awesomewm/widgets.lua ~/.config/awesome/widgets.lua
--- awesomewm/zenburn/icons.lua
+++
@@ -1,9 +0,0 @@
-
--- {{{ Icons
-theme.icon_cpu = "/usr/share/awesome/themes/zenburn/icons/cpu.png"
-theme.icon_ram = "/usr/share/awesome/themes/zenburn/icons/ram.png"
-theme.icon_wifi = "/usr/share/awesome/themes/zenburn/icons/wifi.png"
-theme.icon_hdd = "/usr/share/awesome/themes/zenburn/icons/hdd.png"
-theme.icon_volume = "/usr/share/awesome/themes/zenburn/icons/volume.png"
-theme.icon_battery = "/usr/share/awesome/themes/zenburn/icons/battery.png"
-theme.icon_timedate = "/usr/share/awesome/themes/zenburn/icons/clock.png"
