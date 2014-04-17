2380924665e664b5286649f7a1ae952ee732f189
Jens Jensen <jeggsen@gmail.com>
2014-04-17 16:54:15 +0200

    added a couple of seperators

--- awesomewm/rc.lua
+++ awesomewm/rc.lua
@@ -97,9 +97,11 @@ for s = 1, screen.count() do
    local left_layout = wibox.layout.fixed.horizontal()
    left_layout:add(mytaglist[s])
    left_layout:add(mypromptbox[s])
+   left_layout:add(myseperator)
 
    -- Widgets that are aligned to the right
    local right_layout = wibox.layout.fixed.horizontal()
+   right_layout:add(myseperator)
    if s == 1 then right_layout:add(wibox.widget.systray()) end
 
    -- add widgets    
