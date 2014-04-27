713ed227cc5ac53f307330ab2d3838eb2d6479de
Jens Jensen <jeggsen@gmail.com>
2014-04-27 18:52:17 +0200

    removed unused mouse click events in awesomewm

--- awesomewm/rc.lua
+++ awesomewm/rc.lua
@@ -111,23 +111,7 @@ arrr_ld:set_image(beautiful.arrr_ld)
 mywibox = {}
 mypromptbox = {}
 mytaglist = {}
-mytaglist.buttons = awful.util.table.join(awful.button({ }, 1, awful.tag.viewonly))
 mytasklist = {}
-mytasklist.buttons = awful.util.table.join(
-   awful.button({ }, 1, function (c)
-         if c == client.focus then
-            c.minimized = true
-         else
-            -- Without this, the following :isvisible() makes no sense
-            c.minimized = false
-            if not c:isvisible() then
-               awful.tag.viewonly(c:tags()[1])
-            end
-            -- This will also un-minimize the client, if needed
-            client.focus = c
-            c:raise()
-         end
-end))
 
 for s = 1, screen.count() do
    -- Create a promptbox for each screen
