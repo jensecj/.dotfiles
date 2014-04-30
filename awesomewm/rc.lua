33118eb4f4e9e344cf4873724390d1d7aa2068e3
Jens Jensen <jeggsen@gmail.com>
2014-04-30 14:57:05 +0200

    use a layout that keeps the widgets in the center

--- awesomewm/rc.lua
+++ awesomewm/rc.lua
@@ -6,6 +6,7 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
 local vicious = require("vicious")
+local base = require("wibox.layout.base")
 
 -- use a sane icon size for notifications
 naughty.config.presets.normal.icon_size = 50
@@ -211,7 +212,45 @@ for s = 1, screen.count() do
    right_layout:add(timedate_widget)
 
    -- Now bring it all together (with the tasklist in the middle)
-   local layout = wibox.layout.align.horizontal()
+   -- Create a layout that gives the center widget priority
+   local layout =  setmetatable({}, {__index = wibox.layout.align.horizontal()})
+
+   function layout:draw(wibox, cr, width, height)
+      local size_first = 0
+      local size_third = 0
+      local size_limit = width
+
+      if self.first then
+         local w, h, _ = width, height, nil
+
+         w, _ = base.fit_widget(self.first, w, h)
+         size_first = w
+
+         base.draw_widget(wibox, cr, self.first, 0, 0, w, h)
+      end
+
+      if self.third and size_first < size_limit then
+         local w, h, x, y, _
+
+         w, h = width - size_first, height
+         w, _ = base.fit_widget(self.third, w, h)
+         x, y = width - w, 0
+         size_third = w
+
+         base.draw_widget(wibox, cr, self.third, x, y, w, h)
+      end
+
+      if self.second and size_first + size_third < size_limit then
+         local x, y, w, h
+
+         w, h = size_limit - size_first - size_third, height
+         local real_w, real_h = base.fit_widget(self.second, w, h)
+         x, y = width / 2 - real_w / 2, 0
+         w = real_w
+
+         base.draw_widget(wibox, cr, self.second, x, y, w, h)
+      end
+   end
 
    layout:set_left(left_layout)
    layout:set_middle(middle_layout)
