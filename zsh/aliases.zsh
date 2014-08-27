1dc8758745a3d76deb7d298b5ad0ea0e51784fa7
Jens Jensen <jeggsen@gmail.com>
2014-08-27 14:08:23 +0200

    remove first line in ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 alias ls='ls --color=always --time-style="+%d-%m-%Y" --group-directories-first'
 
-l() { ls -gohX $* | tr -s " " | cut -d" " -f3- | sed "/ /s//\t/" }
-ll() { ls -AgohX $* | tr -s " " | cut -d" " -f3- | sed "/ /s//\t/" }
+l() { ls -gohX $* | sed -n "1!p" | tr -s " " | cut -d" " -f3- | sed "/ /s//\t/" }
+ll() { ls -AgohX $* | sed -n "1!p" | tr -s " " | cut -d" " -f3- | sed "/ /s//\t/" }
 
 alias _='sudo'
 alias ..='cd ..'
