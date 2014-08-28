6bfc85ab6dacd28f63f166ebc9743089fc394bed
Jens Jensen <jeggsen@gmail.com>
2014-08-28 15:25:02 +0200

    cleaned up and added comments

--- zsh/jens-theme.zsh
+++ zsh/jens-theme.zsh
@@ -1,20 +1,21 @@
 autoload colors; colors;
 
-PROMPT='$(_user_host)${_current_dir} $(git_prompt_info)
-$ '
-
-local _current_dir="%{$fg_bold[blue]%}%3~%{$reset_color%} "
-
 function _user_host() {
     if [[ -n $SSH_CONNECTION ]]; then
         me="%n@%m"
     elif [[ $LOGNAME != $USER ]]; then
         me="%n"
     fi
+
     if [[ -n $me ]]; then
         echo "%{$fg[cyan]%}$me%{$reset_color%}:"
     fi
 }
+
+local _current_dir="%{$fg_bold[blue]%}%3~%{$reset_color%} "
+
+PROMPT='$(_user_host) ${_current_dir} $(git_prompt_info)
+> '
 
 # mark dirty repos
 ZSH_THEME_GIT_PROMPT_DIRTY="*"
--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,8 +1,7 @@
-setopt auto_pushd
-setopt auto_cd
-setopt cdablevarS
-setopt pushd_ignore_dups
-setopt pushdminus
-setopt auto_name_dirs
-setopt multios
-setopt prompt_subst
+setopt AUTO_CD # if you just type the name of a directory, cd into it
+setopt AUTO_PUSHD # make cd push the old dir onto the dir stack
+setopt CDABLE_VARS # try to expand arguments to cd if nothing matches
+setopt PUSHD_IGNORE_DUPS # don't push duplicates onto the dir stack
+setopt AUTO_NAME_DIRS # allows naming dirs as variables
+setopt MULTIOS # perform implicit tees or cats when multiple redirections are attempted
+setopt PROMPT_SUBST # pretty prompts
