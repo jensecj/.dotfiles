b10d20eeb834436d51bd23ea06a1cb1df4abff9d
Jens Jensen <jensecj@gmail.com>
2017-03-18 16:03:20 +0100

    added configuration for tmux

--- make-links.sh
+++ make-links.sh
@@ -1,6 +1,8 @@
 # setup config files
 
 ln -s $(pwd)/zsh/zshrc ~/.zshrc
+ln -s $(pwd)/zsh/zprofile ~/.zprofile
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
 
 # this fails if git has already been configured, which is better than
 # overwriting configurations i guess.
