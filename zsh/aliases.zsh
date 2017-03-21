f8ebed7241886ada2c14863afad15528e4615e30
Jens Jensen <jensecj@gmail.com>
2017-03-21 20:43:59 +0100

    updated terminal aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,15 +1,20 @@
+# better ls
 alias ls='ls --color=always --time-style="+%d-%m-%Y" --group-directories-first'
-
 alias l="ls -gholX"
 alias ll="ls -agholX"
 
+# some very common shortcuts
 alias _='sudo'
 alias ..='cd ..'
 
+# a collection of random commands
+
 alias netsw='sudo netctl switch-to'
-alias ytdl="youtube-dl"
-alias ytmp3='youtube-dl -x --audio-format mp3'
-alias dl='aria2c'
+
+alias ytdl='youtube-dl -o"%(uploader)s -- %(title)s.%(ext)s"'
+alias ytmp3='youtube-dl -x --audio-format mp3 -o"%(uploader)s -- %(title)s.%(ext)s"'
+
+alias dl='snatch --threads 4'
 
 # alias git='hub'
 
@@ -18,3 +23,7 @@ alias yau='ya -Syua'
 alias yarm='ya -Rns'
 alias yass='ya -Ss'
 alias yas='ya -S'
+
+alias octave='octave-cli' # who uses the gui anyway?
+
+alias rcp="rsync --verbose --progress"
