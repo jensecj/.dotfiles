60dc43561a3bb7287e5e90f5b4cb091c56faf5ea
Jens Jensen <jensecj@gmail.com>
2017-03-21 20:41:28 +0100

    updated the symlink script

--- make-links.sh
+++ make-links.sh
@@ -1,9 +1,10 @@
 # setup config files
 
-ln -s $(pwd)/zsh/zshrc ~/.zshrc
-ln -s $(pwd)/zsh/zprofile ~/.zprofile
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/zsh/zshrc $HOME/.zshrc
+ln -s $(pwd)/zsh/zprofile $HOME/.zprofile
+ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+
 
 # this fails if git has already been configured, which is better than
 # overwriting configurations i guess.
-ln -s $(pwd)/gitconfig ~/.gitconfig 
+ln -s $(pwd)/gitconfig $HOME/.gitconfig
