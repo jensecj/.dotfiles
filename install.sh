0d98d042346aaec9ac8a96c473daa462ea7ff5e4
Jens <jensecj@gmail.com>
2017-04-14 17:20:00 +0200

    added config for mpv (video player)

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ pacman -S yaourt \
        emacs zsh tmux git hub \
        rust octave jdk8-openjdk jre8-openjdk \
        adobe-source-code-pro-fonts \
-       youtube-dl rsync redshift unclutter keychain
+       youtube-dl rsync redshift unclutter keychain \
+       mpv
 
 cargo install snatch
--- make-links.sh
+++ make-links.sh
@@ -4,8 +4,12 @@ ln -s $(pwd)/Xresources $HOME/.Xresources
 ln -s $(pwd)/zsh/zshrc $HOME/.zshrc
 ln -s $(pwd)/zsh/zprofile $HOME/.zprofile
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+
 ln -s $(pwd)/i3-config $HOME/.i3/config
 ln -s $(pwd)/i3status.conf $HOME/.i3status.conf
+
+ln -s $(pwd)/mpv/input.conf $HOME/.config/mpv/input.conf
+ln -s $(pwd)/mpv/mpv.conf $HOME/.config/mpv/mpv.conf
 
 
 # this fails if git has already been configured, which is better than
