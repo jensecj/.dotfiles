13a96e0a25f669be92fd7ad00900b0948c50a7b1
Jens <jensecj@gmail.com>
2017-04-14 10:48:07 +0200

    updated install scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-pacman -S yaourt emacs zsh rust hub octave youtube-dl \
-       tmux
+pacman -S yaourt \
+       emacs zsh tmux git hub \
+       rust octave jdk8-openjdk jre8-openjdk \
+       adobe-source-code-pro-fonts \
+       youtube-dl rsync redshift unclutter keychain
 
 cargo install snatch
--- make-links.sh
+++ make-links.sh
@@ -1,10 +1,16 @@
 # setup config files
 
+ln -s $(pwd)/Xresources $HOME/.Xresources
 ln -s $(pwd)/zsh/zshrc $HOME/.zshrc
 ln -s $(pwd)/zsh/zprofile $HOME/.zprofile
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/i3-config $HOME/.i3/config
 
 
 # this fails if git has already been configured, which is better than
 # overwriting configurations i guess.
 ln -s $(pwd)/gitconfig $HOME/.gitconfig
+
+# add our binaries to the system, this requires sudo
+ln -s $(pwd)/bin/em /usr/bin/em
+ln -s $(pwd)/bin/colortest /usr/bin/colortest
