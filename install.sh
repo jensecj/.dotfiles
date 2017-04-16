d9cea602e910afd637c47228a202cf72b901f20b
Jens <jensecj@gmail.com>
2017-04-16 11:23:10 +0200

    replaced i3bar with polybar

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ pacman -S yaourt \
        rust octave jdk8-openjdk jre8-openjdk \
        adobe-source-code-pro-fonts \
        youtube-dl rsync redshift unclutter keychain \
-       mpv rofi
+       mpv rofi polybar
 
 cargo install snatch
--- make-links.sh
+++ make-links.sh
@@ -13,6 +13,7 @@ ln -s $(pwd)/mpv/mpv.conf $HOME/.config/mpv/mpv.conf
 
 ln -s $(pwd)/compton.conf $HOME/.config/compton.conf
 ln -s $(pwd)/rofi-config $HOME/.config/rofi/config
+ln -s $(pwd)/polybar $HOME/.config/polybar/config
 
 # this fails if git has already been configured, which is better than
 # overwriting configurations i guess.
