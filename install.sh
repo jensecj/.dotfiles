142be6e05105a968f116b43fe5e4fdb74e0f9ef2
Jens <jensecj@gmail.com>
2017-09-20 15:56:48 +0200

    updated some settings

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 pacman -S yaourt \
-       emacs zsh tmux git hub \
-       rust octave jdk8-openjdk jre8-openjdk \
-       adobe-source-code-pro-fonts \
+       emacs zsh tmux git hub pew \
+       rust octave jdk8-openjdk jre8-openjdk python\
+       adobe-source-code-pro-fonts adobe-source-sans-pro-fonts \
        ttf-material-icons nerd-fonts-complete \
        youtube-dl rsync redshift unclutter keychain \
        mpv rofi polybar
