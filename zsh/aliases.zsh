c1ec3bc923a954fc44cdd0e647ecb92e10ae6ddd
Jens <jensecj@gmail.com>
2017-09-20 15:55:34 +0200

    updated aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,23 +7,34 @@ alias ll="ls -agholXN"
 alias _='sudo'
 alias ..='cd ..'
 
-# a collection of random commands
-
-alias netsw='sudo netctl switch-to'
-
-alias ytdl='youtube-dl -o"%(uploader)s -- %(title)s.%(ext)s"'
-alias ytmp3='youtube-dl -x --audio-format mp3 -o"%(uploader)s -- %(title)s.%(ext)s"'
-
-alias dl='snatch --threads 4'
-
-# alias git='hub'
-
+# easy pacman
 alias ya='yaourt'
 alias yau='ya -Syua'
 alias yarm='ya -Rns'
 alias yass='ya -Ss'
 alias yas='ya -S'
 
+# misc
 alias octave='octave-cli' # who uses the gui anyway?
 
 alias rcp="rsync --verbose --progress"
+alias rmv="rsync --verbose --progress --remove-source-files"
+
+alias pf="peerflix --start --mpv"
+alias dl='snatch --threads 4'
+alias ytdl='youtube-dl -o"%(uploader)s -- %(title)s.%(ext)s"'
+alias ytmp3='youtube-dl -x --audio-format mp3 -o"%(uploader)s -- %(title)s.%(ext)s"'
+
+alias fm="ranger"
+alias sl="streamlink --player=mpv"
+alias myt="mpv --ytdl"
+alias py="python"
+
+alias start_dnscrypt="_ dnscrypt-proxy /etc/dnscrypt-proxy.conf"
+
+alias cl++="clang++ -std=c++14"
+
+# functions
+function ccc {
+    clear && clang++ -std=c++14 $1 && time cat test.in | ./a.out
+}
