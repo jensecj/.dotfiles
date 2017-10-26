79bd477ed91a9bd93231bb78b2f305af7373de87
Jens <jensecj@gmail.com>
2017-10-26 20:00:19 +0200

    started using termite as my terminal emulator

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 pacman -S yaourt \
-       emacs zsh tmux git hub pew \
        rust octave jdk8-openjdk jre8-openjdk python\
+       emacs termite zsh tmux git hub pew \
        adobe-source-code-pro-fonts adobe-source-sans-pro-fonts \
        ttf-material-icons nerd-fonts-complete \
        youtube-dl rsync redshift unclutter keychain \
