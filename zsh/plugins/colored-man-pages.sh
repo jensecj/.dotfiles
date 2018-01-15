8358c95dda5021b36dd438419d4aa031d9213c74
Jens <jensecj@gmail.com>
2018-01-15 11:54:38 +0100

    better colors in man pages. (also more comments)

--- zsh/plugins/colored-man-pages.sh
+++ zsh/plugins/colored-man-pages.sh
@@ -17,17 +17,29 @@ EOF
 fi
 
 function man() {
+    # restrict man-page to a width of 100 columns, makes it a lot easier to read
+    # on big screens
     local width="${COLUMNS:-100}"
     (( width > 100 )) && width=100
 
+    # termcap terminology:
+    # mb = start blink
+    # md = start bold
+    # me = turn off bold, blink and underline
+    # so = start standout
+    # se = stop standout
+    # us = start underline
+    # ue = stop underline
+
+    # set man-page colors to something resembling zenburn
     env \
         LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-        LESS_TERMCAP_md=$(printf "\e[1;31m") \
+        LESS_TERMCAP_md=$(printf "\e[01;38;5;223m") \
         LESS_TERMCAP_me=$(printf "\e[0m") \
+        LESS_TERMCAP_so=$(printf "\e[01;38;5;108m") \
         LESS_TERMCAP_se=$(printf "\e[0m") \
-        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
         LESS_TERMCAP_ue=$(printf "\e[0m") \
-        LESS_TERMCAP_us=$(printf "\e[1;32m") \
+        LESS_TERMCAP_us=$(printf "\e[01;38;5;174m") \
         PAGER="${commands[less]:-$PAGER}" \
         _NROFF_U=1 \
         PATH="$HOME/bin:$PATH" \
