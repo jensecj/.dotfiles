d361dad3732c6152192d30c408b4d3779cfd09c3
Jens Christian Jensen <jensecj@gmail.com>
2018-04-16 16:14:08 +0200

    change default timeout for system boot/shutdown

--- install.sh
+++ install.sh
@@ -131,4 +131,8 @@ cargo install snatch # threaded downloader
 # disable beeps by not loading the pcspkr module
 echo "blacklist pcspkr" >> /etc/modprobe.d/pcspkr-blacklist.conf
 
+# dont wait for a minute and a half before force stopping things
+sed -i 's/#DefaultTimeoutStartSec=90s/DefaultTimeoutStartSec=30s/' /etc/systemd/system.conf
+sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=30s/' /etc/systemd/system.conf
+
 systemctl enable lock-on-sleep
