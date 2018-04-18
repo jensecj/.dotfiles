ceecf599f7c099ac7807a5d544c840973918591e
Jens Christian Jensen <jensecj@gmail.com>
2018-04-18 13:16:58 +0200

    small changes that may be reverted

--- bin/define.sh
+++ bin/define.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 termite --name="dict-lookup" --geometry 700x1000 -e "bash -c 'dict $1| less'"
+# remove define from rofi's history
+# or use ctrl-enter to force current text to be used, instead of first entry
+# sed -i '/\define/d' /home/jens/.cache/rofi-3.runcache
