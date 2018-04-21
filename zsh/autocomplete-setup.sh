cbf1a5bd9d7cab525308f586fe27d02488ae4425
Jens Christian Jensen <jensecj@gmail.com>
2018-04-21 16:18:23 +0200

    cache compinit, and rebuild if older than 24h

--- zsh/autocomplete-setup.sh
+++ zsh/autocomplete-setup.sh
@@ -1,6 +1,14 @@
 # load the auto completion system and setup
 autoload -Uz compinit
-compinit
+
+if [[ $(find /home/jens/.zcompdump -mtime +1) ]]; then
+    compinit
+fi
+
+compinit -C
+
+zstyle ':completion:*' use-cache on
+# zstyle ':completion:*' cache-path ~/.zsh/cache
 # show the selected item in the menu
 zstyle ':completion:*' menu select
 # setup an autocompletion style (case-insensitive)
