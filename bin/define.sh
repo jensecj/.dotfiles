53fce4a2d12d12c472e2cfbb40857c9eb9ec60a8
Jens Christian Jensen <jensecj@gmail.com>
2018-11-14 10:06:41 +0100

    --geometry was removed from termite, fix size of `define` in i3.conf

--- bin/define.sh
+++ bin/define.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # save each word, so they can be used for statistics/practice/etc.
 echo $(date +'%Y-%m-%dT%H:%M:%S%z') " | " $1 >> ~/.dict-lookups
-termite --name="dict-lookup" --geometry 700x1000 -e "bash -c 'dict $1| less'"
+termite --name="dict-lookup" -e "bash -c 'dict $1| less'"
 # remove define from rofi's history
 # or use ctrl-enter to force current text to be used, instead of first entry
 # sed -i '/\define/d' /home/jens/.cache/rofi-3.runcache
