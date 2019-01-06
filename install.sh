7d5eb90f5ba41f9167de61a5f05eb51f9f3a845d
Jens Christian Jensen <jensecj@gmail.com>
2019-01-06 14:52:09 +0100

    split tweak parts from install.sh into post-install.sh

--- install.sh
+++ install.sh
@@ -145,22 +145,3 @@ declare -a rust_packages=(
     snatch # threaded downloader
 )
 cargo install ${rust_packages[@]}
-
-
-##########
-# Tweaks #
-##########
-
-mkdir $HOME/.rtorrent.session
-ln -s $(pwd)/systemd-units/lock-on-sleep.service /etc/systemd/system/
-
-# disable beeps by not loading the pcspkr module
-echo "blacklist pcspkr" >> /etc/modprobe.d/pcspkr-blacklist.conf
-
-# dont wait for a minute and a half before force stopping things
-sed -i 's/#DefaultTimeoutStartSec=90s/DefaultTimeoutStartSec=30s/' /etc/systemd/system.conf
-sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=30s/' /etc/systemd/system.conf
-
-systemctl enable lock-on-sleep.service
-systemctl --user enable random-wallpaper.timer
-systemctl --user start random-wallpaper.timer
---
+++ post-install.sh
@@ -0,0 +1,14 @@
+mkdir $HOME/.rtorrent.session
+ln -s $(pwd)/systemd-units/lock-on-sleep.service /etc/systemd/system/
+
+# disable beeps by not loading the pcspkr module
+echo "blacklist pcspkr" >> /etc/modprobe.d/pcspkr-blacklist.conf
+
+# dont wait for a minute and a half before force stopping things
+sed -i 's/#DefaultTimeoutStartSec=90s/DefaultTimeoutStartSec=30s/' /etc/systemd/system.conf
+sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=30s/' /etc/systemd/system.conf
+
+# enable custom systemd units
+systemctl enable lock-on-sleep.service
+systemctl --user enable random-wallpaper.timer
+systemctl --user start random-wallpaper.timer
