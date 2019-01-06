841449bcbbbfe1c5a4079a01c72c24bdaf8bfd8f
Jens Christian Jensen <jensecj@gmail.com>
2019-01-06 14:53:44 +0100

    created stow.sh, for one-shot setup of dotfiles

---
+++ stow.sh
@@ -0,0 +1,7 @@
+sudo stow -t / root # system config, /etc, /usr, etc.
+stow alacritty termite zsh tmux # shell things
+stow i3 dunst compton polybar # system look and feel
+stow gnupg systemd # system utils
+stow emacs mpv bat cookiecutter ranger rofi rtorrent urlview zathura  # programs
+
+stow misc # user config, bash dotfiles, etc.
