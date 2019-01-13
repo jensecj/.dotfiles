6e74fc8ff86912068b1b5e6f643f8b85b282403e
Jens Christian Jensen <jensecj@gmail.com>
2019-01-13 17:41:40 +0100

    created script to download the mpv plugins i use, until mpv can load submodules

---
+++ mpv/.config/mpv/pull-plugins.sh
@@ -0,0 +1,17 @@
+if [[ ! -f scripts/mpv_thumbnail_script_client_osc.lua ]]; then
+    echo "downloading plugin: mpv_thumbnail_script"
+    curl -s https://api.github.com/repos/TheAMM/mpv_thumbnail_script/releases/latest | grep browser_download_url | cut -d '"' -f 4 | wget -i -
+    cp mpv_thumbnail_script_server.lua scripts/mpv_thumbnail_script_server-a.lua
+    mv mpv_thumbnail_script_server.lua scripts/mpv_thumbnail_script_server-b.lua
+    mv mpv_thumbnail_script_client_osc.lua scripts/mpv_thumbnail_script_client_osc.lua
+fi
+
+if [[ ! -f scripts/youtube-quality.lua ]]; then
+    echo "downloading plugin: mpv-youtube-quality"
+    curl https://raw.githubusercontent.com/jgreco/mpv-youtube-quality/master/youtube-quality.lua > scripts/youtube-quality.lua
+fi
+
+if [[ ! -f scripts/reload.lua ]]; then
+    echo "downloading plugin: reload"
+    curl https://raw.githubusercontent.com/4e6/mpv-reload/master/reload.lua > scripts/reload.lua
+fi
