6518698b1a62008de1a99957aea0556144b2f891
Jens Christian Jensen <jensecj@gmail.com>
2019-08-25 12:15:40 +0200

    added firefox user.js file

---
+++ firefox/user.js
@@ -0,0 +1,4 @@
+user_pref("browser.tabs.insertAfterCurrent", true);
+user_pref("privacy.resistFingerprinting", true);
+user_pref("geo.enabled", false);
+user_pref("general.smoothScroll.currentVelocityWeighting", 0.55)
