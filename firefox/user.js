77efeb87ae6951b0620171a8ec53c6f1e4f840d8
Jens Christian Jensen <jensecj@gmail.com>
2019-09-05 13:16:35 +0200

    updated firefox user.js

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,6 @@
 user_pref("browser.tabs.insertAfterCurrent", true);
 user_pref("privacy.resistFingerprinting", true);
 user_pref("geo.enabled", false);
-user_pref("general.smoothScroll.currentVelocityWeighting", 0.55)
+user_pref("general.smoothScroll.currentVelocityWeighting", "0.55");
+user_pref("privacy.firstparty.isolate", true);
+
