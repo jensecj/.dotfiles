a914a8c0af5a01b234c5bdd6b5f80a9ced46b33e
Jens Christian Jensen <jensecj@gmail.com>
2019-09-05 17:17:45 +0200

    updated firefox user.js

--- firefox/user.js
+++ firefox/user.js
@@ -3,4 +3,7 @@ user_pref("privacy.resistFingerprinting", true);
 user_pref("geo.enabled", false);
 user_pref("general.smoothScroll.currentVelocityWeighting", "0.55");
 user_pref("privacy.firstparty.isolate", true);
+user_pref("extensions.htmlaboutaddons.discover.enabled", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+
 
