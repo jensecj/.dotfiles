7938dcde6475afe6705bfe12ae9dcd7e7edbd51f
Jens Christian Jensen <jensecj@gmail.com>
2019-09-10 12:00:55 +0200

    make sure to ignore duplicates in history

--- zsh/.zsh/options.zsh
+++ zsh/.zsh/options.zsh
@@ -4,7 +4,10 @@ setopt INC_APPEND_HISTORY # Write to the history file immediately, not when the 
 setopt SHARE_HISTORY # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS # delete old duplicated entries
+setopt HIST_FIND_NO_DUPS # dont display lines already found in history
 setopt HIST_IGNORE_SPACE # dont remember commands starting with space, this allows us to keep some commands private
+setopt HIST_VERIFY # verify before executing selected history entry
 
 setopt AUTO_CD # if you just type the name of a directory, cd into it
 setopt AUTO_PUSHD # make cd push the old dir onto the dir stack
