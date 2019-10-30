b82ce6a434c83eac753fb5f74862453c2bbb499a
Jens Christian Jensen <jensecj@gmail.com>
2019-10-30 16:55:09 +0100

    updated firefox user.js

--- firefox/user.js
+++ firefox/user.js
@@ -5,5 +5,7 @@ user_pref("general.smoothScroll.currentVelocityWeighting", "0.55");
 user_pref("privacy.firstparty.isolate", true);
 user_pref("extensions.htmlaboutaddons.discover.enabled", false);
 user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.fullscreen.autohide", false);
+user_pref("dom.event.clipboardevents.enabled", false) // dont tell websites if i copy/paste things
 
 
