d0eebd7cf21cfc0d71d6fbde329eb2a412931170
Jens Christian Jensen <jensecj@posteo.net>
2019-11-14 13:46:50 +0100

    firefox user.js: disable webgl and p2p, remove fullscreen warning

--- firefox/user.js
+++ firefox/user.js
@@ -7,5 +7,8 @@ user_pref("extensions.htmlaboutaddons.discover.enabled", false);
 user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 user_pref("browser.fullscreen.autohide", false);
 user_pref("dom.event.clipboardevents.enabled", false) // dont tell websites if i copy/paste things
+user_pref("webgl.disabled", true)
+user_pref("full-screen-api.warning.timeout", 0) // dont show popup when going fullscreen
+user_pref("media.peerconnection.enabled", false)
 
 
