f7c3e29c48f0ab19e4315d757c05e9821f20aa0e
Jens Christian Jensen <jensecj@posteo.net>
2020-03-03 12:42:40 +0100

    add shred command to ranger

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -7,7 +7,7 @@ class tar(Command):
     def execute(self):
         """ Compress marked files in current buffer """
         cwd = self.fm.thisdir
-        marked_files = cwd.get_selection()
+        marked_files = cwd.get_selection() or []
 
         if not marked_files:
             return
@@ -18,7 +18,7 @@ class tar(Command):
 
         original_path = cwd.path
         parts = self.line.split()
-        args = parts[1:]
+        args = parts[1:] if parts else ""
 
         desc = "archiving files in: " + os.path.basename(parts[1])
         obj = CommandLoader(
@@ -37,6 +37,57 @@ class tar(Command):
     def tab(self):
         cwd = self.fm.thisdir.path
         return str(os.path.basename(cwd)) + ".tar.gz"
+
+
+class shred(Command):
+    def execute(self):
+        """ Compress marked files in current buffer """
+        if self.rest(1):
+            self.fm.notify(
+                "Error: shred takes no arguments! It shreds the selected file(s).",
+                bad=True,
+            )
+            return
+
+        cwd = self.fm.thisdir
+        cf = self.fm.thisfile
+
+        many_files = cwd.marked_items or (
+            cf.is_directory and not cf.is_link and len(os.listdir(cf.path)) > 0
+        )
+
+        marked_files = cwd.get_selection() or cf
+
+        if not marked_files:
+            self.fm.notify("Error: no file selected for shredding!", bad=True)
+            return
+
+        self.fm.ui.console.ask(
+            "Confirm shredding of: %s (y/n)"
+            % ", ".join(f.basename for f in marked_files),
+            self._question_callback,
+        )
+
+    def _question_callback(self, answer):
+        if answer.lower() != "y":
+            return
+
+        original_path = self.fm.thisdir
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        marked_files = self.fm.thisdir.get_selection() or self.fm.thisfile
+
+        args = ["shred", "-u"] + [os.path.abspath(f.path) for f in marked_files]
+        obj = CommandLoader(args=args, read=True, descr="shred files")
+        obj.signal_bind("after", refresh)
+
+        self.fm.loader.add(obj)
+        self.fm.notify("shredding!")
+
+
 class fzf_select(Command):
     """
     :fzf_select
