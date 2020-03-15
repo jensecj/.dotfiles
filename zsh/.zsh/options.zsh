64fdf983abb34c4af3e97c5076e4032ff94c7f69
Jens Christian Jensen <jensecj@posteo.net>
2020-03-15 16:27:34 +0100

    updated zsh options

--- zsh/.zsh/options.zsh
+++ zsh/.zsh/options.zsh
@@ -1,3 +1,4 @@
+# * history
 setopt EXTENDED_HISTORY # Write the history file in the ':start:elapsed;command' format.
 setopt APPEND_HISTORY # append new sessions to the current hist file
 setopt INC_APPEND_HISTORY # Write to the history file immediately, not when the shell exits.
@@ -8,6 +9,17 @@ setopt HIST_IGNORE_ALL_DUPS # delete old duplicated entries
 setopt HIST_FIND_NO_DUPS # dont display lines already found in history
 setopt HIST_IGNORE_SPACE # dont remember commands starting with space, this allows us to keep some commands private
 setopt HIST_VERIFY # verify before executing selected history entry
+
+# * completion
+setopt LIST_TYPES # append trailing type identifiers when listing files
+setopt COMPLETE_ALIASES # also complete switches for aliases
+
+# * misc
+setopt NO_BEEP # no beeping
+unsetopt HIST_BEEP
+setopt LONG_LIST_JOBS
+setopt INTERACTIVECOMMENTS # allow comments in interactive shells
+setopt MARK_DIRS # append trailing `/' when listing directories
 
 setopt AUTO_CD # if you just type the name of a directory, cd into it
 setopt AUTO_PUSHD # make cd push the old dir onto the dir stack
