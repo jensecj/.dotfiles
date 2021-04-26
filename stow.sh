cb76babfbd86023a7e820bb9666c3f896cb382ab
Jens C. Jensen <jens@subst.net>
2021-04-26 11:37:59 +0200

    stow: added some comments on which files require hardlinks

--- stow.sh
+++ stow.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# reflector needs to be a hardlink
+ln $(realpath root/etc/xdg/reflector/reflector.conf) /etc/xdg/reflector/reflector.conf
+# for some reason dictd needs a hardlink
+ln $(realpath dict/etc/dict/dictd.conf) /etc/dict/dictd.conf
+
 sudo stow -t / root # system config, /etc, /usr, etc.
 stow bin
 stow alacritty termite zsh tmux # shell things
 stow i3 dunst compton polybar # system look and feel
 stow gnupg systemd # system utils
 stow emacs mpv bat cookiecutter ranger rofi rtorrent zathura  # programs
+
 
 stow misc # user config, bash dotfiles, etc.
