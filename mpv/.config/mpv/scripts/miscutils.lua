cc3140be2777ed926280f1dd9900de7935c350da
Jens C. Jensen <jensecj@posteo.net>
2021-10-30 14:43:55 +0200

    mpv: miscutils.lua: make sure to use unix clipboard, and not selections; show chapter titles

--- mpv/.config/mpv/scripts/miscutils.lua
+++ mpv/.config/mpv/scripts/miscutils.lua
@@ -3,22 +3,26 @@ local utils = require 'mp.utils'
 
 msg.info("v0.1 loaded")
 
+-- * helpers
+
 local function set_clipboard(text)
-   mp.commandv("run", "/bin/bash", "-c", "printf '"..text.."' | xsel");
+   mp.commandv("run", "/bin/bash", "-c", "printf '"..text.."' | xsel --clipboard");
 end
+
+local function get_clipboard()
+   -- local args = { 'xclip', '-selection', 'clipboard', '-out' }
+   local args = { 'xsel', "--clipboard", "-o" }
+   return utils.subprocess({ args = args, cancellable = false }).stdout
+end
+
+-- * commands
 
 function copyurl()
    local url = mp.get_property("path")
    set_clipboard(url)
    mp.osd_message("copied "..url)
 end
-
 mp.add_key_binding("Ctrl+u", "copyurl", copyurl)
-
-local function get_clipboard()
-   local args = { 'xclip', '-selection', primary and 'primary' or 'clipboard', '-out' }
-   return utils.subprocess({ args = args, cancellable = false }).stdout
-end
 
 function addurl()
    local url = get_clipboard()
@@ -30,5 +34,13 @@ function addurl()
       mp.osd_message("added "..url)
    end
 end
-
 mp.add_key_binding("Ctrl+a", "addurl", addurl)
+
+-- * misc
+
+-- ** show chapter title when it changes
+function display_chapter_title()
+   local title = mp.get_property_osd("chapter-metadata/by-key/title")
+   mp.osd_message(title, 3)
+end
+mp.observe_property("chapter", nil, display_chapter_title)
