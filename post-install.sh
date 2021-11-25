62186f1074953db771ead10070d144684338be63
Jens C. Jensen <jensecj@posteo.net>
2021-11-25 16:53:14 +0100

    post-install: fixed firefox profile install; removed unused services; added misc section

--- post-install.sh
+++ post-install.sh
@@ -1,45 +1,30 @@
 #!/bin/sh
 
-# TODO: auto-detect firefox profile?
-FIREFOX_PROFILE="cf5z4a52.dev-edition-default"
-ln -s "$(pwd)/firefox/user.js" "$HOME/.mozilla/firefox/$FIREFOX_PROFILE/user.js"
-ln -s "$(pwd)/firefox/userChrome.css" "$HOME/.mozilla/firefox/$FIREFOX_PROFILE/chrome/userChrome.css"
-ln -s "$(pwd)/firefox/userContent.css" "$HOME/.mozilla/firefox/$FIREFOX_PROFILE/chrome/userContent.css"
+# auto-detect profiles for installed firefox instances, and link config files
+for profile in $(find "$HOME/.mozilla/firefox" \
+                      -maxdepth 1 \
+                      -type l,d \
+                      -iname '*default*' \
+                     | grep -v 'backup'); do
+    mkdir -p "$profile/chrome"
+    ln -s "$(pwd)/firefox/user.js" "$profile/user.js"
+    ln -s "$(pwd)/firefox/userChrome.css" "$profile/chrome/userChrome.css"
+    ln -s "$(pwd)/firefox/userContent.css" "$profile/chrome/userContent.css"
+done
 
-# use a fancier diff
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-
-mkdir "$HOME/.rtorrent.session"
-
-# disable beeps by not loading the pcspkr module
-echo "blacklist pcspkr" >> /etc/modprobe.d/pcspkr-blacklist.conf
-
-# disable xdg-user-dirs
-echo "enabled=false" >> ~/.config/xdg-user-dirs.conf
-
-# dont wait for a minute and a half before force stopping things
+# don't wait for a minute and a half before force stopping things
 sed -i 's/#DefaultTimeoutStartSec=90s/DefaultTimeoutStartSec=30s/' /etc/systemd/system.conf
 sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=30s/' /etc/systemd/system.conf
 
-systemctl enable dnscrypt-proxy.service
-systemctl start dnscrypt-proxy.service
-
 # get the network up
-systemctl enable iwd
-systemctl enable dhcpcd
-
-# enable custom systemd units
-systemctl enable lock-on-sleep.service
-
-systemctl --user enable random-wallpaper.timer
-systemctl --user enable lowtmp
-systemctl --user enable mbsync
-
-systemctl --user start random-wallpaper.timer
-systemctl --user start lowtmp
-systemctl --user start mbsync
+systemctl enable --now iwd
+systemctl enable --now dhcpcd
+systemctl enable --now dnscrypt-proxy.service
 
 # fix fork bombs
 if [ ! $(grep "\* hard nproc" /etc/security/limits.conf) ]; then
     echo "* hard nproc 2048" >> /etc/security/limits.conf
 fi
+
+# misc
+mkdir "$HOME/.rtorrent.session"
